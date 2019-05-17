b5f7aec0bf21cdfc6d21c9048383ced266ca5f54
Anna Nowak <annadnowak98@gmail.com>
2019-05-18 01:01:33 +0200

    Repaired mute-button

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -7,17 +7,20 @@ public class AudioManager : MonoBehaviour
     // Start is called before the first frame update
     AudioSource audioSource;
     bool isPlaying = true;
+    
+
     void Start()
     {
-        audioSource = gameObject.GetComponent<AudioSource>();
         if (FindObjectsOfType<AudioManager>().Length > 1)
             Destroy(gameObject);
         DontDestroyOnLoad(this.gameObject);
+        audioSource = gameObject.GetComponent<AudioSource>();
     }
-
+    public bool IsPlaying => isPlaying;
     public void PlayOrMute()
     {
-        if(isPlaying)
+        
+        if (isPlaying)
         {
             audioSource.Pause();
         }
---
+++ Assets/Scripts/ToggleButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleButton : MonoBehaviour
+{
+    Toggle toggleBar;
+    AudioManager audioManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        toggleBar = gameObject.GetComponent<Toggle>();
+        audioManager = FindObjectOfType<AudioManager>();
+        toggleBar.isOn = audioManager.IsPlaying;
+    }
+    public void PlayOrMute()
+    {
+        audioManager.PlayOrMute();
+        toggleBar.isOn = audioManager.IsPlaying;
+    }
+}
